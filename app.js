5231c4bdda83b96944c11aa516c1412445c2ce60
Thomas Iseli <thomas.iseli@webyourself.ch>
2021-03-07 14:54:56 +0100

    created separate areas for auth and public

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const customers = require('./routes/customers');
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
-});
+app.use(express.static('public'));
+
+app.use('/customers', customers);
 
 app.listen(port, () => {
 	console.log(`Gandalf is live at http://localhost:${port}, baby!`);
