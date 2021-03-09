5860ad92d83366cdbb1efca339f8ea9aa11be35a
Thomas Iseli <thomas.iseli@webyourself.ch>
2021-03-09 22:24:53 +0100

    configured dummy login page

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const open = require('open');
 const customers = require('./routes/customers');
 
 const app = express();
 const port = 3000;
 
+app.engine('handlebars', exphbs({ extname: 'hbs', defaultLayout: 'app.hbs' }));
+app.set('view engine', 'hbs');
+
 app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+	res.render('login');
+});
 
 app.use('/customers', customers);
 
