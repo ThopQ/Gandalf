4c67521d176de604c7eae51bb978188ed978d4d3
Thomas Iseli <thomas.iseli@webyourself.ch>
2021-03-21 21:39:33 +0100

    styling

--- routes/customers.js
+++ routes/customers.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-// middleware that is specific to this router
-router.use(function timeLog(req, res, next) {
-	console.log('time', Date.now());
-	next();
-});
 // define the home page route
 router.get('/', function (req, res) {
 	res.render('customers', { layout: 'app.hbs' });
+});
+
+router.get('/:id/computers', function (req, res) {
+	//res.render('customers', { layout: 'app.hbs' });
+	res.send(`Test ID: ${req.params.id}`);
 });
 
 module.exports = router;
