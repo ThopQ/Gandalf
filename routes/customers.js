95186596fb6d7e9a85a1be5ad6c294e021e94c95
Thomas Iseli <thomas.iseli@webyourself.ch>
2021-03-28 14:20:29 +0200

    now renders view and sends data to html

--- routes/customers.js
+++ routes/customers.js
@@ -1,15 +1,15 @@
 //setup router as mini-app in express
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 //get customers route for showing all customers after login
-router.get("/", (req, res) => {
-  res.render("customers", { layout: "app.hbs" });
+router.get('/', (req, res) => {
+	res.render('customers', { layout: 'app.hbs' });
 });
 
 //get computers route with id parameter from customer
-router.get("/:id/computers", (req, res) => {
-  res.send(`Test ID: ${req.params.id}`);
+router.get('/:id/computers', (req, res) => {
+	res.render('computers', { layout: 'app.hbs', customerId: req.params.id });
 });
 
 module.exports = router;
